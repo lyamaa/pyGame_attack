587f59baf32c431d26bf35ab18735ecf5498702d
surajmt8848 <you@example.com>
2021-06-02 09:45:13 +0545

    object movement using keystroke

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("img/spacecraft.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
 
 def player(x, y):
@@ -28,11 +30,28 @@ running = True
 while running:
     # RGB
     screen.fill((0, 255, 0))
-    playerY -= 0.1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("Left arroe pressed")
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                print("Right key pressed")
+                playerX_change = 0.1
+        if event.type == pygame.KEYUP:
 
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("KEY Released")
+                playerX_change = 0
+                playerY_change = 0
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+    playerY += playerY_change
     player(playerX, playerY)
     pygame.display.update()
