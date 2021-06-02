4f8f4023fbc3af37d1af518311a7e72d5e14fa97
surajmt8848 <you@example.com>
2021-06-02 10:25:40 +0545

    enemy movememt

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ playerY_change = 0
 # Enemy
 enemyImg = pygame.image.load("img/cthulhu.png")
 enemyX = random.randint(0, 800)
-enemyY = random.randint(10, 150)
-enemyX_change = 0
-enemyY_change = 0
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 def player(x, y):
@@ -64,6 +64,14 @@ while running:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    enemyX += enemyX_change
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+
     playerY += playerY_change
     player(playerX, playerY)
     enemy(enemyX, enemyY)
