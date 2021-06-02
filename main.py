085b376e2bca213dd510a03394681e6b77a2fe3c
surajmt8848 <you@example.com>
2021-06-02 08:47:25 +0545

    title, icons and color

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.init()
 # Create Screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Attacks")
+icon = pygame.image.load("img/rocket.png")
+pygame.display.set_icon(icon)
 
 # GAME LOOP
 running = True
@@ -15,3 +19,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # RGB
+    screen.fill((255, 255, 255))
+    pygame.display.update()
